7a2c0cb284085cb5222a4244743e08ec61d79b85
Alexia <alexheg44@gmail.com>
2021-03-21 21:04:52 -0400

    commit tree ee3eb9bf904a71b06e80725dd452c5398d7c4a03

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+
+class MP3Importer
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files 
+        full_files = Dir.glob("#{self.path}/*.mp3")
+        @files = full_files.collect {|file| file = File.basename(file)}
+  end
+
+    def import
+        self.files.each {|filename| Song.new_by_filename(filename)}
+    end
+end
